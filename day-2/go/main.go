e61ed464b2fb2c5e933720e0c4879f6e0c7c0660
Lois <loisisar@outlook.com>
2023-12-02 21:35:42 +0000

    day 2 midway

--- day-2/go/main.go
+++ day-2/go/main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+var Requirement = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
+func formatAndCalculateSet(set string) int {
+
+	// At this point the cube will be 1 red, 1 blue, 4 green
+	cubes := strings.Split(set, ",")
+	cubMap := make(map[string]int, len(cubes))
+	for index, cubeColor := range cubes {
+		// At this point the cube will be one of 1 red, 1 blue, 4 green
+		color := strings.Split(cubeColor, " ")[1]
+		numberOfCube := strconv.Atoi(strings.Split(cubeColor, " ")[0])
+	}
+
+	// just put return 0 for now
+	return 0
+}
+
+func main() {
+	file, err := os.ReadFile("../input.txt")
+	if err != nil {
+		fmt.Println("Error reading file")
+	}
+	myFile := string(file[:])
+	lines := strings.Split(myFile, "\n")
+	for _, line := range lines {
+
+		game := strings.Split(line, ":")[0]
+
+		// get the game id's number here
+		gameId, err := strconv.Atoi(strings.Split(game, " ")[1])
+
+		if err != nil {
+			fmt.Println("Atoi failed", err)
+		}
+		// the second part of each lines are the sets of games
+		games := strings.Split(line, ":")[1]
+		for _, sets := range games {
+			// convert sets' rune value to string
+			setString := string(sets)
+			// split the sets by ;
+			set := strings.Split(setString, ";")
+
+		}
+
+	}
+}
