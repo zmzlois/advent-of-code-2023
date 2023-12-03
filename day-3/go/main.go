f4ce744e8ce3a0f80087697c2a70f250f7741e08
Lois <loisisar@outlook.com>
2023-12-03 22:36:30 +0000

    index out of range...

--- day-3/go/main.go
+++ day-3/go/main.go
@@ -10,25 +10,28 @@ var symbols = []string{"*", "/", "-", "&", "$", "=", "+", "@", "#", "%"}
 var numbers = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
 
 func ifCharIsNumb(HashMap [][]string, i int, j int, char string, numb string) {
-	var HashOne string
-	var HashTwo string
+
+	var numbSets = make([]string, 3)
 	if char == numb {
+		numbSets[0] = char
 		if j < len(HashMap[i]) {
 			for _, numbOne := range numbers {
 				if HashMap[i][j+1] == numbOne {
-
-					HashOne = numbOne
+					numbSets[1] = HashMap[i][j+1]
 					for _, numbTwo := range numbers {
 						if HashMap[i][j+2] == numbTwo {
+							numbSets[2] = HashMap[i][j+2]
 
-							HashTwo = numbTwo
 						}
+						continue
 					}
 				}
+				continue
 			}
 
 		}
 	}
+	fmt.Printf("row %d: numbSet: %v\n", i, numbSets)
 }
 func main() {
 	file, _ := os.ReadFile("../input.txt")
@@ -36,7 +39,7 @@ func main() {
 
 	lines := strings.Split(outputFile, "\n")
 	HashMap := make([][]string, len(lines))
-	numbStore := make([]int, 0)
+	// numbStore := make([]int, 0)
 	for i, line := range lines {
 		// store them in a hashMap first
 		HashMap[i] = strings.Split(line, "")
@@ -47,13 +50,12 @@ func main() {
 			// loop through the symbols and store the position in HashMap
 
 			for _, numb := range numbers {
-				if char == numb {
+				ifCharIsNumb(HashMap, i, j, char, numb)
 
-				}
 			}
 
 		}
 	}
-	fmt.Println("Hashmap?", HashMap)
+	// fmt.Println("Hashmap?", HashMap)
 
 }
