9735d3c2c150bafe9dfd233fcf73aa9199b5c4db
Lois <loisisar@outlook.com>
2023-12-03 10:10:51 +0000

    trimmed white space finally get the correct bool value

--- day-2/go/main.go
+++ day-2/go/main.go
@@ -16,15 +16,18 @@ var Requirement = map[string]int{
 
 func formatAndCheckingValidSet(set string) bool {
 
+	fmt.Println("Set looks like", set)
 	// At this point the cube will be 1 red, 1 blue, 4 green
 	cubes := strings.Split(set, ",")
 	cubeMap := make(map[string]int, len(cubes))
 	for _, cubeColor := range cubes {
 		// At this point the cube will be one of 1 red, 1 blue, 4 green
-		parts := strings.Split(cubeColor, " ")
-
-		if len(parts) < 2 {
-			// fmt.Println("Invalid cubColor format: ", cubeColor)
+		// There are white space around the character so needs to trim it
+		trimmedCubeColor := strings.TrimSpace(cubeColor)
+		parts := strings.Split(trimmedCubeColor, " ")
+		if len(parts) != 2 {
+			fmt.Println("Invalid parts format: ", parts)
+			fmt.Println("Length of parts", len(parts))
 
 			continue
 		}
@@ -45,12 +48,22 @@ func formatAndCheckingValidSet(set string) bool {
 			cubeMap[color] = updatedNumber
 
 		}
+		fmt.Printf("cubMap: %d %v", cubeMap[color], color)
 		if cubeMap[color] > Requirement[color] {
 			return false
 		}
 
 	}
 
+	return true
+}
+
+func CheckSetOk(boolSet []bool) bool {
+	for _, bools := range boolSet {
+		if bools == false {
+			return false
+		}
+	}
 	return true
 }
 
@@ -62,28 +75,24 @@ func main() {
 	myFile := string(file[:])
 	lines := strings.Split(myFile, "\n")
 
-	var allGameID []int
+	// var allGameID []int
 	for _, line := range lines {
-		
 
 		game := strings.Split(line, ":")
-		fmt.Println("games", game)
 
 		if len(game) != 2 {
 			fmt.Println("length of game", len(game))
-			continue 
+			continue
 		}
 
-		
+		// Game name: Game 89
 		gameName := game[0]
-
-		fmt.Println("Game name:", gameName)
+		// 6 green, 5 blue; 4 green, 4 blue; 3 red, 5 blue
 		gameSets := game[1]
-		fmt.Println("Game Sets: ", gameSets)
 
 		gameNameSplit := strings.Split(gameName, " ")
 
-		if len(gameNameSplit) < 2 {
+		if len(gameNameSplit) != 2 {
 			fmt.Println("Game name split error", gameNameSplit)
 		}
 		gameId, IdErr := strconv.Atoi(gameNameSplit[1])
@@ -92,20 +101,22 @@ func main() {
 			fmt.Println("Atoi error parsing gameId", gameId)
 		}
 
-		for _, sets := range gameSets {
+		gameSet := strings.Split(gameSets, ";")
+
+		for _, sets := range gameSet {
+			// declare a slice to store ok
+			var okSlices []bool
 			// convert sets' rune value to string
 			setString := string(sets)
-			// split the sets by ;
-			set := strings.Split(setString, ";")
-			for _, smallset := range set {
 
-				if ok := formatAndCheckingValidSet(smallset); ok {
-					allGameID = append(allGameID, gameId)
-				}
-			}
+			ok := formatAndCheckingValidSet(setString)
+			okSlices = append(okSlices, ok)
+			fmt.Println("ok", ok)
+			fmt.Println("okSlice", okSlices)
 
 		}
 
-	} // fmt.Println("Game ID Set", allGameID)
+	}
+	// fmt.Println("Game ID Set", allGameID)
 
 }
