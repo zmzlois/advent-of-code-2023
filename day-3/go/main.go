14c6c2b275ec42f27f2c3f3bb4dd9baa82708abc
Lois <loisisar@outlook.com>
2023-12-04 07:00:52 +0000

    convert string to digit and make sure the are the correct number

--- day-3/go/main.go
+++ day-3/go/main.go
@@ -3,35 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 var symbols = []string{"*", "/", "-", "&", "$", "=", "+", "@", "#", "%"}
 var numbers = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
 
-func ifCharIsNumb(HashMap [][]string, i int, j int, char string, numb string) {
+var symbolIndex = make(map[int][]int)
 
-	var numbSets = make([]string, 3)
-	if char == numb {
-		numbSets[0] = char
-		if j < len(HashMap[i]) {
-			for _, numbOne := range numbers {
-				if HashMap[i][j+1] == numbOne {
-					numbSets[1] = HashMap[i][j+1]
-					for _, numbTwo := range numbers {
-						if HashMap[i][j+2] == numbTwo {
-							numbSets[2] = HashMap[i][j+2]
+// for a digit array looks like [2, 3, 4] and they should be joined together as a single digit
+func convertDigit(digitString []string) int {
+	var digitArray []int
+	result := 0
+	for _, digit := range digitString {
+		if len(digit) <= 1 {
+			continue
+		}
+		digitConvert, digitErr := strconv.Atoi(digit)
+		if digitErr != nil {
+			fmt.Println("error converting digit")
+		}
+		digitArray = append(digitArray, digitConvert)
+	}
+	// for every number in this updated digit array, make sure they become a real, single number
+	for _, digit := range digitArray {
+		result = result*10 + digit
+	}
+	fmt.Println("convertedDigit:", result)
+	return result
+}
 
-						}
-						continue
-					}
-				}
-				continue
-			}
+// for every position of the symbol, unless it is first line, check the line above
+// here we want to get an array of strings looks like [432 573 418 318]
+func checkLineAbove(HashMap [][]string, symbolIndex map[int][]int, i int, j int) []string {
 
+	var result []string
+	return result
+}
+
+// Make symbol position index
+func GetSymbolIndex(HashMap [][]string, i int, j int, char string) map[int][]int {
+	// var lineIndex []int
+
+	for _, symbol := range symbols {
+		if char == symbol {
+			symbolIndex[i] = append(symbolIndex[i], j)
 		}
 	}
-	fmt.Printf("row %d: numbSet: %v\n", i, numbSets)
+	return symbolIndex
 }
 func main() {
 	file, _ := os.ReadFile("../input.txt")
@@ -48,14 +68,11 @@ func main() {
 
 		for j, char := range chars {
 			// loop through the symbols and store the position in HashMap
-
-			for _, numb := range numbers {
-				ifCharIsNumb(HashMap, i, j, char, numb)
-
-			}
+			symbolIndex = GetSymbolIndex(HashMap, i, j, char)
 
 		}
 	}
+	// fmt.Println("symbolIndex:", symbolIndex)
 	// fmt.Println("Hashmap?", HashMap)
 
 }
